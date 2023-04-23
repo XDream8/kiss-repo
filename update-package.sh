e4673113a93338fc7aa0e4185c2425ddfd7851a5
XDream8 <ism.frat@hotmail.com>
2023-04-23 19:55:51 +0300

    update-script: update-package.sh

--- update-package.sh
+++ update-package.sh
@@ -10,12 +10,22 @@ help() {
 }
 
 update_version() {
-	ver="$(cut -d' ' -f1 < version)"
+	ver="$(cut -d' ' -f1 <version)"
 
 	sed -i "s/$ver/$package_version/g" sources
 	sed -i "s/$ver/$package_version/g" version
 
-	kiss c 1>/dev/null
+	kiss c >/dev/null && printf '%s\n' "Generated checksums" || {
+		printf '%s\n' "Generating checksums failed, reverting chamges"
+		sed -i "s/$package_version/$ver/g" sources
+		sed -i "s/$package_version/$ver/g" version
+		exit 1
+	}
+}
+
+git_commit() {
+	git add sources version checksums
+	git commit -o sources version checksums -m "$package_name: bump to version $package_version"
 }
 
 main() {
@@ -27,9 +37,15 @@ main() {
 	[ "$package_path" ] || help
 	[ "$package_version" ] || help
 
+	package_name="${package_path%/}"
+	package_name="${package_name#./}"
+	package_name="${package_name#*/}"
+
 	cd "$package_path" || exit
 
 	update_version
+
+	git_commit
 
 	cd "$current_dir" || exit
 }
